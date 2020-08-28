6101335dd406aba108020ee430c58d1fa61384d5
Hoàng Tùng <54903350+hoangtung97@users.noreply.github.com>
2020-08-28 08:40:27 +0700

    .

---
+++ Electric Software/form menu/Lience.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace form_menu
+{
+    public partial class Lience : Form
+    {
+        int mov;
+        int movX;
+        int movY;
+        public Lience()
+        {
+            InitializeComponent();
+        }
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mov = 1;
+            movX = e.X;
+            movY = e.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mov == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - movX, MousePosition.Y - movY);
+            }
+        }
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mov = 0;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReadWriter rd = new ReadWriter();
+            if (textBox1.Text == "admin" && textBox2.Text == "dien123123")
+            {
+                rd.SaveLienceKey();
+                MessageBox.Show("MÁY TÍNH ĐÃ ĐƯỢC CẤP QUYỀN THÀNH CÔNG!!(=_+)");
+
+
+            }
+            else MessageBox.Show("THÔNG TIN TÀI KHOẢN HOẶC MẬT KHẨU KHÔNG CHÍNH XÁC");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
---
+++ Electric Software/form menu/bin/Debug/SECURITY.e
@@ -0,0 +1,5 @@
+<?xml version="1.0"?>
+<LienceKey xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <ID_PC>4C361A355ECE4B531D97105E932F7382</ID_PC>
+  <date>AE5B7D8943E8F7BBE8BA57B327DAAA05</date>
+</LienceKey>
